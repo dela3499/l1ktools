dela3499/l1ktools

commit 54f43ac0944759a23537b23a5cecb7a9bdc2d457

fixed bug to account for case when one field is requrested and some docs are None

python/cmap/util/api_utils.py
@@ -316,6 +316,13 @@ class CMapAPI(object):
 			return res
 		# if not, see if there's just one field
 		else:
+			# if just one field, first check if any of the returned documents are None, 
+			# meaning they didn't contain the one requested field
+			if len(query_args['fields']) == 1:
+				if any([not(x) for x in res]):
+					res = [x for x in res if x]
+					if self.verbose:
+						print 'only {0} documents contained the one requested field'.format(len(res))
 			# Check that all requested fields were found
 			if query_args['fields'] is not None:
 				missings = self._check_fields(res, query_args['fields'])
@@ -325,7 +332,7 @@ class CMapAPI(object):
 					if toDataFrame:
 						res = pd.DataFrame(res)[field]
 					else:
-						res = [x[field] for x in res]
+						res = [x[field] for x in res if x]
 					return res
 			# if more fields, convert to data frame
 			if toDataFrame: res = pd.DataFrame(res)
@@ -335,7 +342,7 @@ class CMapAPI(object):
 		'''
 		Check that all requested fields were returned in the query result.
 		'''
-		allfields = sorted(set([z for y in [x.keys() for x in res] 
+		allfields = sorted(set([z for y in [x.keys() for x in res if x] 
 		                   		for z in y]))
 		missings = sorted(np.setdiff1d(fields, allfields))
 		if missings:
@@ -346,7 +353,7 @@ class CMapAPI(object):
 		# if verbose is set, notify if some fields were not found in all docs
 		if self.verbose:
 			indiv_missing = [np.setdiff1d(allfields, x.keys())
-							 for x in res]
+							 for x in res if x]
 			indiv_missing = sorted(set([z for y in indiv_missing 
 			                       		for z in y]))
 			if indiv_missing:

commit 1c7900c12f924ceef3cd392f92c3648e1c28d7e5

fixed bug when there aren't any fields in the query_args
the bug was introduced in 54f43ac0

python/cmap/util/api_utils.py
@@ -316,15 +316,15 @@ class CMapAPI(object):
 			return res
 		# if not, see if there's just one field
 		else:
-			# if just one field, first check if any of the returned documents are None, 
-			# meaning they didn't contain the one requested field
-			if len(query_args['fields']) == 1:
-				if any([not(x) for x in res]):
-					res = [x for x in res if x]
-					if self.verbose:
-						print 'only {0} documents contained the one requested field'.format(len(res))
 			# Check that all requested fields were found
 			if query_args['fields'] is not None:
+				# if just one field, first check if any of the returned documents are None,
+				# meaning they didn't contain the one requested field
+				if len(query_args['fields']) == 1:
+					if any([not(x) for x in res]):
+						res = [x for x in res if x]
+						if self.verbose:
+							print 'only {0} documents contained the one requested field'.format(len(res))
 				missings = self._check_fields(res, query_args['fields'])
 				# if single field, output list / series
 				if (len(query_args['fields']) == 1) and (not missings):